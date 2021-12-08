63f6ca29245c5692d485510695890860d24533e7
plrnvld <paulrijneveld@gmail.com>
2021-12-08 21:30:41 +0000

    Find solution for part 2

--- main.py
+++ main.py
@@ -7,20 +7,28 @@ minCrab = min(crabs)
 maxCrab = max(crabs)
 print(f'Min {minCrab} and max {maxCrab}.')
 
-def calc_fuel(all_crabs,center_pos):
+def sum_naturals(n):
+  return (n*(n+1))/2
+
+def calc_fuel_new_way(x1, x2):
+  return sum_naturals(abs(x1 - x2))
+
+def calc_fuel(all_crabs, center_pos):
   fuel = 0
   for c in all_crabs:
-    fuel += abs(c - center_pos)
+    fuel += calc_fuel_new_way(c, center_pos)
   return fuel
 
-minFuel = len(crabs) * maxCrab
+bestPos = 0
+minFuel = len(crabs) * calc_fuel_new_way(minCrab, maxCrab)
 
 for pos in range(minCrab, maxCrab):
   res = calc_fuel(crabs, pos)
   if res < minFuel:
       minFuel = res
+      bestPos = pos
 
-print(f'Min fuel {minFuel}.')
+print(f'Min fuel {minFuel}, best pos {bestPos}.')
 
 
 
